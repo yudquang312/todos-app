501dd83547dfd268886f6620fcd2777d29a70743
yudquang312 <duyquang31296@gmail.com>
2020-08-21 01:19:45 +0700

    optimize code and change color wrong right question

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -4,101 +4,83 @@ import completed from "../../asset/images/completed.svg";
 import remove from "../../asset/images/remove.svg";
 
 export default class TodoItem extends Component {
-    constructor(props) {
-        super(props);
-        this.textInput = React.createRef();
-        this.state = {
-            inputStatus: false,
-        };
-    }
-
-    // componentDidMount() {
-    //     console.log(this.props.content);
-    //     console.log("check point");
-    //     if (this.state.inputStatus) {
-    //         console.log(this.props.content);
-    //         this.textInput.current.value = this.props.content;
-    //         this.textInput.current.focus();
-    //     }
-    // }
-    componentDidUpdate() {
-        console.log("did update");
-        if (this.state.inputStatus) {
-            this.textInput.current.value = this.props.content;
-            this.textInput.current.focus();
-        }
-    }
-
-    //
-    render() {
-        let { checked, content, onDeleted, onChecked, index } = this.props;
-        let { inputStatus } = this.state;
-        return (
-            <li className={"todo-item"}>
-                <span onClick={() => onChecked(index)}>
-                    <img src={checked ? completed : active} alt="" />
-                </span>
-                {/* <label
-                    className={
-                        (checked ? "text-completed " : "") +
-                        (inputStatus ? "d-none" : "")
-                    }
-                    onDoubleClick={this.onDoubleClick}
-                >
-                    {content}
-                </label>
-                <input
-                    ref={this.textInput}
-                    onKeyUp={this.onKeyUp}
-                    onBlur={this.onBlur}
-                    className={inputStatus ? "" : "d-none"}
-                ></input> */}
-                {inputStatus ? (
-                    <input
-                        autoFocus
-                        ref={this.textInput}
-                        onKeyUp={this.onKeyUp}
-                        onBlur={this.onBlur}
-                    ></input>
-                ) : (
-                    <label
-                        className={checked ? "text-completed " : ""}
-                        onDoubleClick={this.onDoubleClick}
-                    >
-                        {content}
-                    </label>
-                )}
-
-                <button onClick={() => onDeleted(index)}>
-                    <img src={remove} alt="" />
-                </button>
-            </li>
-        );
-    }
-
-    onDoubleClick = (event) => {
-        // console.log("double click");
-        this.setState({
-            inputStatus: true,
-        });
+  constructor(props) {
+    super(props);
+    this.textInput = React.createRef();
+    this.state = {
+      inputStatus: false,
     };
+  }
 
-    onKeyUp = (event) => {
-        if (event.keyCode === 13 || event.key === "Enter") {
-            this.props.onEditItem(
-                this.props.index,
-                this.textInput.current.value
-            );
-            this.setState({
-                inputStatus: false,
-            });
-        }
-    };
+  // componentDidMount() {
+  //     console.log(this.props.content);
+  //     console.log("check point");
+  //     if (this.state.inputStatus) {
+  //         console.log(this.props.content);
+  //         this.textInput.current.value = this.props.content;
+  //         this.textInput.current.focus();
+  //     }
+  // }
+  componentDidUpdate() {
+    console.log("did update");
+    if (this.state.inputStatus) {
+      this.textInput.current.value = this.props.content;
+      this.textInput.current.focus();
+    }
+  }
 
-    onBlur = (event) => {
-        this.props.onEditItem(this.props.index, this.textInput.current.value);
-        this.setState({
-            inputStatus: false,
-        });
-    };
+  //
+  render() {
+    let { checked, content, onDeleted, onChecked, index } = this.props;
+    let { inputStatus } = this.state;
+    return (
+      <li className={"todo-item"}>
+        <span onClick={() => onChecked(index)}>
+          <img src={checked ? completed : active} alt="" />
+        </span>
+        {inputStatus ? (
+          <input
+            autoFocus
+            ref={this.textInput}
+            onKeyUp={this.onKeyUp}
+            onBlur={this.onBlur}
+          ></input>
+        ) : (
+          <label
+            className={checked ? "text-completed " : ""}
+            onDoubleClick={this.onDoubleClick}
+          >
+            {content}
+          </label>
+        )}
+
+        <button onClick={() => onDeleted(index)}>
+          <img src={remove} alt="" />
+        </button>
+      </li>
+    );
+  }
+
+  onDoubleClick = (event) => {
+    // console.log("double click");
+    this.setState({
+      inputStatus: true,
+    });
+  };
+
+  onKeyUp = (event) => {
+    if (event.keyCode === 13 || event.key === "Enter") {
+      this.props.onEditItem(this.props.index, this.textInput.current.value);
+      this.setState({
+        inputStatus: false,
+      });
+    }
+  };
+
+  onBlur = (event) => {
+    this.props.onEditItem(this.props.index, this.textInput.current.value);
+    this.setState({
+      inputStatus: false,
+    });
+  };
 }
--- src/components/TodoWrapper/TodoWrapper.js
+++ src/components/TodoWrapper/TodoWrapper.js
@@ -4,195 +4,164 @@ import TodoItem from "../TodoItem/TodoItem";
 import todoApp from "../../asset/images/todo-app.svg";
 
 export default class TodoWrapper extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            items: JSON.parse(localStorage.getItem("todo-items"))
-                ? JSON.parse(localStorage.getItem("todo-items"))
-                : [],
-            text: "",
-            checkAll: true,
-            view: "all",
-        };
-        this.inputNewItem = React.createRef();
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: JSON.parse(localStorage.getItem("todo-items"))
+        ? JSON.parse(localStorage.getItem("todo-items"))
+        : [],
+      text: "",
+      checkAll: true,
+      view: "all",
+    };
+    this.inputNewItem = React.createRef();
+  }
 
-    componentDidUpdate() {
-        localStorage.setItem("todo-items", JSON.stringify(this.state.items));
-    }
+  componentDidUpdate() {
+    localStorage.setItem("todo-items", JSON.stringify(this.state.items));
+  }
 
-    render() {
-        // console.log("render wrapper");
-        let { items, checkAll } = this.state;
-        let itemsLeft = items.filter((item) => !item.checked).length;
+  render() {
+    // console.log("render wrapper");
+    let { items, checkAll } = this.state;
+    let itemsLeft = items.filter((item) => !item.checked).length;
+    return (
+      <div className="wrapper">
+        <div className="todo-header">
+          <span onClick={() => this.checkedAll(checkAll)}>
+            <img src={todoApp} alt=""></img>
+          </span>
+          <input
+            ref={this.inputNewItem}
+            type="text"
+            autoFocus
+            placeholder="What need to be done?"
+            onKeyUp={this.addNewItem}
+          />
+        </div>
+        <div>
+          <ul className="todo-list">
+            {this.showTodoItem(items, this.state.view)}
+          </ul>
+        </div>
+        {items.length ? (
+          <TodoFooter
+            itemsLeft={itemsLeft}
+            itemsCompleted={items.some((item) => item.checked)}
+            onClearCompleted={this.onClearCompleted}
+            onViewItem={this.onViewItem}
+          />
+        ) : (
+          ""
+        )}
+      </div>
+    );
+  }
+
+  checkedAll = (isChecked) => {
+    this.setState((state) => {
+      return {
+        items: state.items.map((item) => ({ ...item, checked: isChecked })),
+        checkAll: !state.checkAll,
+      };
+    });
+    // localStorage.setItem("todo-items", JSON.stringify(this.state.items));
+  };
+
+  showTodoItem = (items, view) => {
+    let content = [];
+
+    if (view === "all") {
+      content = [...items];
+    } else {
+      if (view === "completed") {
+        content = items.filter((item) => item.checked);
+      } else {
+        content = items.filter((item) => !item.checked);
+      }
+    }
+    return content
+      .map((item, index) => {
         return (
-            <div className="wrapper">
-                <div className="todo-header">
-                    <span onClick={() => this.checkedAll(checkAll)}>
-                        <img src={todoApp} alt=""></img>
-                    </span>
-                    <input
-                        ref={this.inputNewItem}
-                        type="text"
-                        autoFocus
-                        placeholder="What need to be done?"
-                        onKeyUp={this.addNewItem}
-                    />
-                </div>
-                <div>
-                    <ul className="todo-list">
-                        {this.showTodoItem(items, this.state.view)}
-                    </ul>
-                </div>
-                {items.length ? (
-                    <TodoFooter
-                        itemsLeft={itemsLeft}
-                        itemsCompleted={items.length - itemsLeft}
-                        onClearCompleted={this.onClearCompleted}
-                        onViewItem={this.onViewItem}
-                    />
-                ) : (
-                    ""
-                )}
-            </div>
+          <TodoItem
+            key={index}
+            index={index}
+            content={item.content}
+            checked={item.checked}
+            onChecked={this.onChecked}
+            onDeleted={this.onDeleted}
+            onEditItem={this.onEditItem}
+          />
         );
+      })
+      .reverse();
+  };
+
+  addNewItem = (event) => {
+    if (event.key === "Enter" || event.keyCode === 13) {
+      this.setState({
+        items: [
+          ...this.state.items,
+          {
+            content: this.inputNewItem.current.value,
+            checked: false,
+          },
+        ],
+        text: "",
+      });
+      // localStorage.setItem(
+      //     "todo-items",
+      //     JSON.stringify(this.state.items)
+      // );
+      this.inputNewItem.current.value = "";
     }
+  };
 
-    checkedAll = (isChecked) => {
-        this.setState((state) => {
-            return {
-                items: state.items.map((item) => {
-                    return { ...item, checked: isChecked };
-                }),
-                checkAll: !state.checkAll,
-            };
-        });
-        // localStorage.setItem("todo-items", JSON.stringify(this.state.items));
-    };
+  onClearCompleted = () => {
+    let items = this.state.items;
+    items = items.filter((item) => !item.checked);
+    this.setState((state) => {
+      return {
+        items,
+      };
+    });
+    // localStorage.setItem("todo-items", JSON.stringify(this.state.items));
+  };
 
-    showTodoItem = (items, view) => {
-        let content = [];
+  onChecked = (index) => {
+    let items = [...this.state.items];
+    items[index].checked = !items[index].checked;
+    this.setState((state) => {
+      return {
+        items,
+      };
+    });
+    // localStorage.setItem("todo-items", JSON.stringify(this.state.items));
+  };
 
-        if (view === "all") {
-            if (items.length) {
-                content = items.map((item, index) => {
-                    return (
-                        <TodoItem
-                            key={index}
-                            index={index}
-                            content={item.content}
-                            checked={item.checked}
-                            onChecked={this.onChecked}
-                            onDeleted={this.onDeleted}
-                            onEditItem={this.onEditItem}
-                        />
-                    );
-                });
-            }
-        } else {
-            if (view === "completed") {
-                content = items
-                    .filter((item) => item.checked)
-                    .map((item, index) => {
-                        return (
-                            <TodoItem
-                                key={index}
-                                index={index}
-                                content={item.content}
-                                checked={item.checked}
-                                onChecked={this.onChecked}
-                                onDeleted={this.onDeleted}
-                                onEditItem={this.onEditItem}
-                            />
-                        );
-                    });
-            } else {
-                content = items
-                    .filter((item) => !item.checked)
-                    .map((item, index) => {
-                        return (
-                            <TodoItem
-                                key={index}
-                                index={index}
-                                content={item.content}
-                                checked={item.checked}
-                                onChecked={this.onChecked}
-                                onDeleted={this.onDeleted}
-                                onEditItem={this.onEditItem}
-                            />
-                        );
-                    });
-            }
-        }
+  onDeleted = (index) => {
+    let items = [...this.state.items];
+    items.splice(index, 1);
+    this.setState((state) => {
+      return {
+        items,
+      };
+    });
+    // localStorage.setItem("todo-items", JSON.stringify(this.state.items));
+  };
 
-        return content.reverse();
-    };
+  onViewItem = (view) => {
+    this.setState({
+      view,
+    });
+  };
 
-    addNewItem = (event) => {
-        if (event.key === "Enter" || event.keyCode === 13) {
-            this.setState({
-                items: [
-                    ...this.state.items,
-                    {
-                        content: this.inputNewItem.current.value,
-                        checked: false,
-                    },
-                ],
-                text: "",
-            });
-            // localStorage.setItem(
-            //     "todo-items",
-            //     JSON.stringify(this.state.items)
-            // );
-            this.inputNewItem.current.value = "";
-        }
-    };
-
-    onClearCompleted = () => {
-        let items = this.state.items;
-        items = items.filter((item) => !item.checked);
-        this.setState((state) => {
-            return {
-                items,
-            };
-        });
-        // localStorage.setItem("todo-items", JSON.stringify(this.state.items));
-    };
-
-    onChecked = (index) => {
-        let items = [...this.state.items];
-        items[index].checked = !items[index].checked;
-        this.setState((state) => {
-            return {
-                items,
-            };
-        });
-        // localStorage.setItem("todo-items", JSON.stringify(this.state.items));
-    };
-
-    onDeleted = (index) => {
-        let items = [...this.state.items];
-        items.splice(index, 1);
-        this.setState((state) => {
-            return {
-                items,
-            };
-        });
-        // localStorage.setItem("todo-items", JSON.stringify(this.state.items));
-    };
-
-    onViewItem = (view) => {
-        this.setState({
-            view,
-        });
-    };
-
-    onEditItem = (index, content) => {
-        // console.log(index, content);
-        let items = [...this.state.items];
-        items[index].content = content;
-        this.setState({
-            items,
-        });
-    };
+  onEditItem = (index, content) => {
+    // console.log(index, content);
+    let items = [...this.state.items];
+    items[index].content = content;
+    this.setState({
+      items,
+    });
+  };
 }
